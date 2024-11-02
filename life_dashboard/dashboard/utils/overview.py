80d2be232ac8c70dce87f4bfb835841264b9cbc1
Nigel Visser <ndjvisser@veridis.tech>
2024-11-02 19:38:47 +0100

    Update overview.py

--- life_dashboard/dashboard/utils/overview.py
+++ life_dashboard/dashboard/utils/overview.py
@@ -1,3 +1,9 @@
+"""
+This module contains classes that are responsible for calculating trends for
+different aspects of life. Each class is responsible for calculating trends for
+a specific aspect of life.
+"""
+
 
 class HealthOverview:
     def __init__(self, health_stats):
@@ -7,6 +13,7 @@ class HealthOverview:
         # Implement logic to analyze health trends
         pass
 
+
 class WealthOverview:
     def __init__(self, wealth_stats):
         self.wealth_stats = wealth_stats  # This would be a Wealth instance
@@ -15,9 +22,10 @@ class WealthOverview:
         # Implement logic to analyze wealth trends
         pass
 
+
 class RelationshipsOverview:
     def __init__(self, relationship_stats):
-        self.relationship_stats = relationship_stats  # This would be a Relationships instance
+        self.relationship_stats = relationship_stats
 
     def calculate_trends(self):
         # Implement logic to analyze relationship trends
