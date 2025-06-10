a1f576857dee99d9a695a35e573d8b8db1cd6423
Nigel Visser <ndjvisser@veridis.tech>
2025-06-10 20:44:12 +0200

    Configure Django’s MESSAGE_TAGS for Bootstrap alerts

---
+++ life_dashboard/settings.py
@@ -0,0 +1,9 @@
+from django.contrib import messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "secondary",
+    messages.INFO: "info",
+    messages.SUCCESS: "success",
+    messages.WARNING: "warning",
+    messages.ERROR: "danger",
+}
