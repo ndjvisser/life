fd8a81e9f508f8afd7c203bf584eaa497b241d1a
Nigel Visser <62035018+ndjvisser@users.noreply.github.com>
2025-09-21 14:11:30 +0200

    Revert "Add domain services for skill and achievement summaries"

--- life_dashboard/achievements/domain/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Domain layer exports for achievements."""
-
-from .services import AchievementProgressService, build_achievement_progress_response
-
-__all__ = [
-    "AchievementProgressService",
-    "build_achievement_progress_response",
-]
